9bbd53c4c0d5f93caf4db1fd88cee994639cdcb7
lionel-m <lionel.maccaud@gmail.com>
2012-11-23 15:25:46 +0100

    Bug fixes

--- system/modules/superfish/classes/Superfish.php
+++ system/modules/superfish/classes/Superfish.php
@@ -10,12 +10,10 @@
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
-
 /**
  * Run in a custom namespace, so the class can be replaced
  */
 namespace Contao;
-
 
 /**
  * Class Superfish 
@@ -30,22 +28,32 @@ class Superfish extends \Frontend {
         
         $options = array();
         
-        $options[0] = "hoverClass: '".$objLayout->sf_hoverClass."',\n";
-        $options[1] = (($objLayout->sf_pathClass != NULL) ? "pathClass: '".$objLayout->sf_pathClass."',\n" : "");
-        $options[2] = (($objLayout->sf_pathClass != NULL) ? "pathLevels: ".$objLayout->sf_pathLevels.",\n" : "");
-        $options[3] = "delay: ".$objLayout->sf_delay.",\n";
-        $options[4] = (($objLayout->sf_animation != NULL) ? "animation: '".$objLayout->sf_animation."',\n" : "");
-        $options[5] = "speed: ".(is_numeric($objLayout->sf_speed) ? $objLayout->sf_speed.",\n" : "'".$objLayout->sf_speed."',\n");
-        $options[6] = (($objLayout->sf_autoArrows != NULL) ? "autoArrows: ".(($objLayout->sf_autoArrows == true) ? "true,\n" : "false,\n") : "");
-        $options[7] = (($objLayout->sf_dropShadows != NULL) ? "dropShadows: ".(($objLayout->sf_dropShadows == true) ? "true,\n" : "false,\n") : "");
-        $options[8] = (($objLayout->sf_disableHI != NULL) ? "disableHI: ".(($objLayout->sf_disableHI == true) ? "true,\n" : "false,\n") : "");
-        $options[9] = (($objLayout->sf_onInit != NULL) ? "onInit: '".$objLayout->sf_onInit."',\n" : "");
-        $options[10] = (($objLayout->sf_onBeforeShow != NULL) ? "onBeforeShow: '".$objLayout->sf_onBeforeShow."',\n" : "");
-        $options[11] = (($objLayout->sf_onShow != NULL) ? "onShow: '".$objLayout->sf_onShow."',\n" : "");
-        $options[12] = (($objLayout->sf_onHide != NULL) ? "onHide: '".$objLayout->sf_onHide."',\n" : "");
+        $options[0]  = "hoverClass: '".$objLayout->sf_hoverClass."'";
+        $options[1]  = (($objLayout->sf_pathClass != NULL) ? "pathClass: '".$objLayout->sf_pathClass."'" : '');
+        $options[2]  = (($objLayout->sf_pathClass != NULL) ? "pathLevels: ".$objLayout->sf_pathLevels : '');
+        $options[3]  = "delay: ".$objLayout->sf_delay;
+        $options[4]  = (($objLayout->sf_animation != NULL) ? "animation: ".$objLayout->sf_animation : '');
+        $options[5]  = "speed: ".(is_numeric($objLayout->sf_speed) ? $objLayout->sf_speed : "'".$objLayout->sf_speed."'");
+        $options[6]  = (($objLayout->sf_autoArrows == 1) ? '' : "autoArrows: false");
+        $options[7]  = (($objLayout->sf_dropShadows == 1) ? '' : "dropShadows: false");
+        $options[8]  = (($objLayout->sf_disableHI == 1) ? "disableHI: true" : '');
+        $options[9]  = (($objLayout->sf_onInit != NULL) ? "onInit: ".$objLayout->sf_onInit : '');
+        $options[10] = (($objLayout->sf_onBeforeShow != NULL) ? "onBeforeShow: ".$objLayout->sf_onBeforeShow : '');
+        $options[11] = (($objLayout->sf_onShow != NULL) ? "onShow: ".$objLayout->sf_onShow : '');
+        $options[12] = (($objLayout->sf_onHide != NULL) ? "onHide: ".$objLayout->sf_onHide : '');
+        
+        // remove empty value
+        foreach ($options as $key => $value) {
+            if (empty($value)) {
+                unset($options[$key]);
+            }
+        }
         
         // Reindex the array
         $options = array_values($options);
+        
+        // Contert to String
+        $options = implode(",\n", $options);
         
         if($objLayout->superfish == 1) {
             $GLOBALS['TL_MOOTOOLS'][] = 
@@ -56,42 +64,17 @@ class Superfish extends \Frontend {
                     (function($) { ".
                     (($objLayout->supersubs == false) ? "
                         $(\"ul.sf-menu\").superfish({" ."\n".
-                            $options[0].
-                            $options[1].
-                            $options[2].
-                            $options[3].
-                            $options[4].
-                            $options[5].
-                            $options[6].
-                            $options[7].
-                            $options[8].
-                            $options[9].
-                            $options[10].
-                            $options[11].
-                            $options[12].
-                        "});
+                            $options.
+                        " });
                         " : "
                         $(\"ul.sf-menu\").supersubs({ 
                                 minWidth: ".$objLayout->sf_minWidth.",
                                 maxWidth: ".$objLayout->sf_maxWidth.",
                                 extraWidth: ".$objLayout->sf_extraWidth."
                             }).superfish({" ."\n".
-                                $options[0].
-                                $options[1].
-                                $options[2].
-                                $options[3].
-                                $options[4].
-                                $options[5].
-                                $options[6].
-                                $options[7].
-                                $options[8].
-                                $options[9].
-                                $options[10].
-                                $options[11].
-                                $options[12].
-                            "});
-                        ") ."
-                    })(jQuery);
+                                $options.
+                            " });
+                        ") ."})(jQuery);
                 </script>
             ";
         }
